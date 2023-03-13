a498e415a7b60affbe56a49bfc110df1a33a5aa8
AbdElrhman Bakr <abdelrhmanbakr.pc@gmail.com>
2023-03-13 04:25:05 +0200

    Final Back-End Node Only.

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,7 +1,19 @@
-import { Link } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import { userContext } from "../../context/UserContext";
 import "./Intro.css";
 
 const Intro = () => {
+  const navigateTo = useNavigate();
+  const { currentUser } = useContext(userContext);
+
+  useEffect(() => {
+    if (currentUser.id) {
+      navigateTo("/home");
+    }
+  }, [currentUser]);
+
   return (
     <section className="intro-sec">
       <div className="sec-typewriter">
--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -1,10 +1,21 @@
-import { useState } from "react";
+import { useState, useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 import InputBox from "../InputBox/InputBox";
 import ImageRecognition from "../ImageRecognition/ImageRecognition";
+import { userContext } from "../../context/UserContext";
 import "./MainContent.css";
 
 const MainContent = () => {
+  const navigateTo = useNavigate();
+  const { currentUser } = useContext(userContext);
   const [imageURL, setImageURL] = useState("");
+
+  useEffect(() => {
+    if (!currentUser.id) {
+      navigateTo("/");
+    }
+  }, [currentUser]);
 
   return (
     <section>
